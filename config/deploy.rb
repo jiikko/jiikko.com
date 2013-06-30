5bcba8b46dd14f94b7edbd94b3e31310a0d09aa3
jiikko <n905i.1214@gmail.com>
2013-06-30 16:01:46 +0900

    deploy.rbかえた

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,6 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
-
 set :user, 'kjdev'
 set :use_sudo, false
-set :application, 'jiikko.me'
+#set :application, 'jiikko.me'
 
 set :scm, :git
 set :repository, 'git@bitbucket.org:jiikko/jiikko.me.git'
@@ -15,10 +11,10 @@ set :deploy_via, :export
 
 # -*- coding: utf-8 -*-
 set :deploy_to, "/home/kjdev/public_html/jiikko.me"
-set(:app_uri, 'http://jiikko.me/')
-set(:env, 'gehirn rs2')
+#set(:app_uri, 'http://jiikko.me/')
+#set(:env, 'gehirn rs2')
 
-role :app, "s11.rs2.gehirn.jp"
+# role :app, "s11.rs2.gehirn.jp"
 role :web, "s11.rs2.gehirn.jp"
-role :db,  "s11.rs2.gehirn.jp", :primary => true
+# role :db,  "s11.rs2.gehirn.jp", :primary => true
 set :normalize_asset_timestamps, false
